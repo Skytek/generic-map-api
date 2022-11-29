a5251edce9f57743c13e5249d40bf990f53e82e4
Wiktor Latanowicz <wiktor@latanowicz.com>
2022-11-29 16:53:15 +0100

    Fix viewport polygon calculation

--- tests/test_values.py
+++ tests/test_values.py
@@ -5,5 +5,5 @@ from shapely.geometry import Point
 def test_viewport_from_geohashes():
     geohashes = "g823h7/sw096f"
     viewport = ViewPort.from_geohashes_query_param(geohashes)
-    assert viewport.upper_left == Point(-21.961669921875, 46.5985107421875)
-    assert viewport.lower_right == Point(23.3349609375, 33.9862060546875)
+    assert viewport.upper_left == Point(-21.961669921875, 46.60400390625)
+    assert viewport.lower_right == Point(23.3349609375, 33.980712890625)
